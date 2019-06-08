f57705499f2a6742b357d2c7788c3a6640eed50b
Sergey Filippov <sergey.filippov@outlook.com>
2019-06-08 22:29:00 +0200

    encryption tests added

---
+++ steamLauncher.Tests/EncryptionTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests
+{
+    using NUnit.Framework;
+
+    using steamLauncher.Logic.Helpers;
+
+    [TestFixture]
+    public class EncryptionTests
+    {
+        [Test]
+        public void TestCorrectEncryptionCycle()
+        {
+            var originalText = "readable string";
+            var password = "12345678";
+
+            var encrypted = EncryptionHelper.Encrypt(password, originalText);
+            Assert.AreEqual("ZOMscBDLBYJ79h4SkJfBKA==", encrypted);
+
+            var decrypted = EncryptionHelper.Decrypt(password, encrypted);
+            Assert.AreEqual(originalText, decrypted);
+        }
+    }
+}
